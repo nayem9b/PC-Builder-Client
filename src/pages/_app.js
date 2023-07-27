37a93fb31ab7b6deb4fb2c892c61e7503fd7a877
nayem9b <razwanniam1@gmail.com>
2023-07-28 01:53:54 +0600

    redux integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <SessionProvider session={pageProps.session}>
-      <NextUIProvider>{getLayout(<Component {...pageProps} />)}</NextUIProvider>
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider session={pageProps.session}>
+        <NextUIProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </NextUIProvider>
+      </SessionProvider>
+    </Provider>
   );
 }
