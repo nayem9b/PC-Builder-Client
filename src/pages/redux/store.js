96ed7b03b7d8eebff7e3086ab1243c93b3bd5bef
nayem9b <razwanniam1@gmail.com>
2023-07-28 02:02:33 +0600

    redux store persisted

--- src/pages/redux/store.js
+++ src/pages/redux/store.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/api";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const reducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
 
-export default configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+export default store;
